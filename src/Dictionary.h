38a67eea8f71b1ed3f4e883acc125b1bdbc5fce0
Vojtech Horky <vojtech.horky@gmail.com>
2009-05-30 13:06:51 +0000

    Add Doxygen comments

--- src/Dictionary.h
+++ src/Dictionary.h
@@ -3,20 +3,33 @@
 #include <QObject>
 #include <QString>
 
-
+/**
+ * Abstract ancestor for all dictionaries in OtterDict.
+ * 
+ * Due to way MOC treates files you <em>have to</em> specify
+ * the hitFound() signal also in derived classes.
+ * 
+ */
 class Dictionary : public QObject {
 	
 public:
+	/// Constructor.
 	Dictionary() {}
+	/// Virtual destructor.
 	virtual ~Dictionary() {}
+	/// Clones itself, returning new copy.
 	virtual Dictionary * clone() const { return 0; }
+	/// Tells name of the dictionary.
 	virtual QString getName() const { return ""; }
+	/// Called when new search is requested.
 	void setTerm(const QString & str) {
 		translate(str);
 	}
 signals: 
+	/// Signal to be emmited when a translation hit is found.
 	void hitFound(const QString &, const QString &);
 protected:
+	/// Callback for a new search - the heart of the dictionary.
 	virtual void translate(const QString &) {};
 };
 
